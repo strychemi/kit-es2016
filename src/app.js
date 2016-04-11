5c248e8db27f095fa2da67fe3b31c9a31cb9f253
Andrew Baik <and.baik@gmail.com>
2016-04-11 01:53:03 -0400

    users page added

---
+++ src/app.js
@@ -0,0 +1,11 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
+      { route: 'users', name: 'users', moduleId: './users',nav: true, title:'Github Users' }
+    ]);
+
+    this.router = router;
+  }
+}
